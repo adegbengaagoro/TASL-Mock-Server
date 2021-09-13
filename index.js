e6d28aca8f4154cf8bd2b1c9b299c76281b34084
Agoro, Adegbenga. B <adegbenga@designsbycrenet.com>
2021-09-13 17:26:57 +0100

    feat(tasl-style): create the functionality based on the TASL specification

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT
 const fetchTransactionListing = require('./util/fetchTransactionListing')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(`${PORT}`, () => console.log(`TASL Mock Server listening on port ${PORT}`))
 
 // List Transactions
 app.get('/', async (_request, response) => {
@@ -40,4 +47,35 @@ app.get('/fetch/transaction/:transactionID', async (request, response) => {
   })
 })
 
-module.exports = app
+// Fetch Single Transaction by ID using TASL Format
+app.post('/:productApplication/_search', async (request, response) => {
+  let transaction
+  const productApplication = request.params.productApplication
+  const {
+    query: {
+      match: { transactionid: transactionID }
+    }
+  } = request.body
+
+  const { data: transactionListing } = await fetchTransactionListing()
+
+  const transactionIsInArray = transactionListing.some((element) => {
+    return element.hits.hits[0]._source.transaction_id === transactionID
+  })
+
+  if (transactionIsInArray) {
+    transaction = transactionListing.find((element) => {
+      return element.hits.hits[0]._source.transaction_id === transactionID
+    })
+
+    response.status(200).send({
+      data: transaction,
+      product_application: productApplication
+    })
+  }
+
+  response.status(404).json({
+    status: 'Error',
+    message: `Transaction with ID, ${transactionID}, not found!`
+  })
+})
